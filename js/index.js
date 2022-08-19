3eec3ff138cd48925cf23962f2a0c11a84b78a18
Tamila Suleimanova <tamilasu94@gmail.com>
2022-08-20 01:09:15 +0300

    Add first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () { 
+
+    document.querySelector('#burger').addEventListener('click',
+    function () {
+    document.querySelector('#menu').classList.toggle('active')
+    })
+  document.querySelector('#close-menu').addEventListener('click',
+    function () {
+    document.querySelector('#menu').classList.toggle('active')
+  })
+
+})
